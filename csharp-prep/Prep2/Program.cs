80b424efbb0c3268be976d4c40fa536f19bf51c1
sartentooth <sebastiancascallares@gmail.com>
2023-01-07 16:01:18 -0300

    prep2 ready

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Clear();
+        Console.Write("Welcome, please enter your Grade Percentage (0-100): ");
+        string grade = Console.ReadLine();
+        int userInput = int.Parse(grade);   
+
+        string letter = "";
+
+        if (userInput >= 90) 
+        {
+            letter = "A";
+        }
+        else if (userInput >= 80)
+        {
+            letter = "B";
+        }
+        else if (userInput >= 70)
+        {
+            letter = "C";
+        }
+        else if (userInput >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your Letter Grade is: {letter}");
+
+        if (userInput >= 70)
+        {
+            Console.WriteLine("Congratulation! You passed");
+        }
+        else 
+        {
+            Console.WriteLine("You need to do better");            
+        }
+        
     }
 }
